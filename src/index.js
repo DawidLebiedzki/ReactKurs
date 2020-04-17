5d061cccc0ec153773479842c3fa4d9e75066ef3
Dawid Lebiedzki <lebiedzki@gmail.com>
2020-04-17 10:19:05 +0200

    add clock component

--- src/index.js
+++ src/index.js
@@ -1,15 +1,51 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+function Welcome(props) {
+  return <h1>Hello, {props.name}</h1>;
+}
+
+const element = <Welcome name="Sara" />;
+
+class Clock extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { date: new Date() };
+  }
+
+  componentDidMount() {
+    this.ticer = setInterval(() => this.tick(), 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.ticker);
+  }
+
+  tick() {
+    this.setState({
+      date: new Date(),
+    });
+  }
+  render() {
+    return (
+      <div>
+        <h1>Current time: {this.state.date.toLocaleTimeString()}</h1>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<Clock />, document.getElementById("root"));
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
